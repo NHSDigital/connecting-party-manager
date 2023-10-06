09ac11903978a76de816db2f3f74f3b3222f3e74
jameslinnell <james.linnell2@nhs.net>
2023-10-06 14:03:02 +0100

    Bootstrap non mgmt

--- scripts/infrastructure/initialise-non-prod-resources.sh
+++ scripts/infrastructure/initialise-non-prod-resources.sh
@@ -13,6 +13,6 @@ AWS_REGION_NAME="eu-west-2"
 admin_policy_arn="arn:aws:iam::aws:policy/AdministratorAccess"
 
 tf_assume_role_policy=NULL
-tf_assume_role_policy=$(awk "{sub(/REPLACEME/,\"${MGMT_ACCOUNT_ID}\")}1" role-trust-policy.json)
+tf_assume_role_policy=$(awk "{sub(/REPLACEME/,\"${MGMT_ACCOUNT_ID}\")}1" ./scripts/infrastructure/role-trust-policy.json)
 aws iam create-role --role-name "${TERRAFORM_ROLE_NAME}" --assume-role-policy-document "${tf_assume_role_policy}" --region "${AWS_REGION_NAME}" || return 1
 aws iam attach-role-policy --policy-arn "${admin_policy_arn}" --role-name "${TERRAFORM_ROLE_NAME}" --region "${AWS_REGION_NAME}" || return 1
