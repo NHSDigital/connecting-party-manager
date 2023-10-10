ed10e1477ca1267dd44b3b82259db33bd7dfdd04
jameslinnell <james.linnell2@nhs.net>
2023-10-10 16:28:54 +0100

    Terraform local scripts

--- infrastructure/terraform/per_workspace/main.tf
+++ infrastructure/terraform/per_workspace/main.tf
@@ -1,24 +1,3 @@
-provider "aws" {
-  region = local.region
-
-  assume_role {
-    role_arn = "arn:aws:iam::${var.assume_account}:role/${var.assume_role}"
-  }
-
-  default_tags {
-    tags = {
-      Environment      = var.environment
-      Created          = local.created
-      Workspace        = replace(terraform.workspace, "_", "-")
-      Project          = local.project
-      Name             = "${local.project}--${replace(terraform.workspace, "_", "-")}"
-      Owner            = "NHSE"
-      ProjectShortName = "CPM"
-      ProjectFullname  = "Connecting Party Manager"
-    }
-  }
-}
-
 resource "aws_resourcegroups_group" "resource_group" {
   name = "${local.project}--${replace(terraform.workspace, "_", "-")}--resource-group"
   tags = {
