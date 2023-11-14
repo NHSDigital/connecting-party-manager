7e68d3169c68e63f3d6fca7f406faf1f07751d61
Joel Klinger <joel.klinger1@nhs.net>
2023-11-14 14:21:42 +0000

    [release/2023-11-14] bug-fix to changelog checker

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,35 +1,27 @@
 # Changelog
 
 ## 2023-11-14
-
 - [PI-125] FHIR / Swagger / Pydantic generation
 - [PI-000] Dependabot
-- [PI-000] Mask template file
+- [PI-000] debug
 
 ## 2023-11-09-a
-
 - [PI-75] Create general table and repository interface
 
 ## 2023-11-07
-
 - PI-112: Refactor API Gateway Terraform
 
 ## 2023-11-03
-
 - [PI-53] Swagger models and a REST API that calls domain logic
 
 ## 2023-11-02
-
 - [PI-52] ProductTeam Domain logic
 
 ## 2023-11-01
-
 - [PI-107] Destroy workspaces
 
 ## 2023-10-30
-
 - [PI-72] Validate ODS Code
 
 ## 2023-10-27-a
-
 - [PI-71] Created releases
--- scripts/changelog/changelog-precommit.sh
+++ scripts/changelog/changelog-precommit.sh
@@ -3,15 +3,19 @@
 branch=$(git symbolic-ref --short HEAD)
 if [[ "${branch}" == *"release/"* ]]; then
     result=`python ./scripts/changelog/changelog_precommit.py $branch`
-    if [[ "${result}" == "CHANGELOG.md updated." ]]; then
-        echo "SUCCESS: CHANGELOG.md was out of date and has now been updated. Please re-commit."
+    if [[ "${result}" == "updated" ]]; then
+        echo "SUCCESS: CHANGELOG.md has now been updated: please re-commit."
         exit 1
-    elif [[ "${result}" == "No matching changelog for release branch." ]]; then
-        echo "FAILURE: You must create a change log to match the release branch."
+    elif [[ "${result}" == "file_not_found" ]]; then
+        echo "FAILURE: File not found- changelog/${branch}.md"
         exit 1
-    else
+    elif [[ "${result}" == "nothing_to_do" ]]; then
         exit 0
+    else
+        echo ${result}
+        exit 1
     fi
 else
+    echo "Not a release branch, skipping"
     exit 0
 fi
