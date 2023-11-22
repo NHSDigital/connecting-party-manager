225e8975b576a13e376c6d1eeadd07be43eb2f32
Joel Klinger <joel.klinger1@nhs.net>
2023-11-22 14:53:23 +0000

    [release/2023-11-22] Fix-forward: rm poetry lock

--- scripts/poetry/poetry.mk
+++ scripts/poetry/poetry.mk
@@ -1,14 +1,15 @@
 .PHONY: poetry--update poetry--install poetry--clean
 
 VENV_PYTHON = $(CURDIR)/.venv/bin/python
-VENV_TIMESTAMP = $(TIMESTAMP_DIR)/.venv.stamp
 PYPROJECT_TOML = $(CURDIR)/pyproject.toml
+POETRY_LOCK = $(CURDIR)/poetry.lock
 
-poetry--update: $(VENV_TIMESTAMP) ## Updates installed dependencies as specified in pyproject.toml
+poetry--update: $(POETRY_LOCK) ## Updates installed dependencies as specified in pyproject.toml
 poetry--install: $(VENV_PYTHON) ## First time installation of poetry configuration
 
 poetry--clean:  ## Remove .venv directory
 	[[ -d .venv ]] && rm -r .venv || :
+	[[ -f $(POETRY_LOCK) ]] && rm $(POETRY_LOCK) || :
 
 $(VENV_PYTHON):
 	poetry -q || (pip install --upgrade pip && pip install poetry)
@@ -17,6 +18,6 @@ $(VENV_PYTHON):
 	.venv/bin/pre-commit install
 	touch $(VENV_PYTHON)
 
-$(VENV_TIMESTAMP): $(TIMESTAMP_DIR) $(VENV_PYTHON) $(PYPROJECT_TOML)
+$(POETRY_LOCK): $(TIMESTAMP_DIR) $(VENV_PYTHON) $(PYPROJECT_TOML)
 	poetry update
-	touch $(VENV_TIMESTAMP)
+	touch $(POETRY_LOCK)
