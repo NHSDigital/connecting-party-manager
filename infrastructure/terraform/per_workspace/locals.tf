15cdd8f5ae8999d148fe66a7d6afea61276225f6
jameslinnell <james.linnell2@nhs.net>
2023-11-29 14:25:10 +0000

    Resource Group description

--- infrastructure/terraform/per_workspace/locals.tf
+++ infrastructure/terraform/per_workspace/locals.tf
@@ -2,5 +2,5 @@ locals {
   region         = "eu-west-2"
   project        = "nhse-cpm"
   current_time   = timestamp()
-  workspace_type = contains(terraform.workspace, "dev") || contains(terraform.workspace, "ref") || contains(terraform.workspace, "int") || contains(terraform.workspace, "prod") ? "${var.workspace_type} ${upper(replace(terraform.workspace, "_", "-"))}" : "${var.workspace_type}"
+  workspace_type = strcontains(terraform.workspace, "dev") || strcontains(terraform.workspace, "ref") || strcontains(terraform.workspace, "int") || strcontains(terraform.workspace, "prod") ? "${var.workspace_type} ${upper(replace(terraform.workspace, "_", "-"))}" : "${var.workspace_type}"
 }
