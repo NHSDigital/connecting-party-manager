56bb91ba5557a34e46c14a5df51954f2c551997b
jameslinnell <james.linnell2@nhs.net>
2023-12-27 14:59:32 +0000

    unit test fixes

--- src/api/readDevice/tests/test_index.py
+++ src/api/readDevice/tests/test_index.py
@@ -3,6 +3,7 @@ import os
 from unittest import mock
 
 import pytest
+from domain.core.device_key import DeviceKeyType
 from domain.core.root import Root
 from domain.repository.device_repository import DeviceRepository
 from nhs_context_logging import app_logger
@@ -20,15 +21,14 @@ TABLE_NAME = "hiya"
     ],
 )
 def test_index(version):
-    device_id = "XXX-YYY"
+    device_key = "P.XXX-YYY"
     org = Root.create_ods_organisation(ods_code="ABC")
     product_team = org.create_product_team(
         id=consistent_uuid(1), name="product-team-name"
     )
-    device = product_team.create_device(
-        id=device_id, name="device-name", type="product"
-    )
-    device.add_key(type="product_id", key=device_id)
+    device = product_team.create_device(name="device-name", type="product")
+    device.add_key(DeviceKeyType.PRODUCT_ID, device_key)
+    device_id = device.id
 
     with mock_table(TABLE_NAME) as client, mock.patch.dict(
         os.environ,
@@ -61,7 +61,7 @@ def test_index(version):
                 }
             },
             "identifier": [
-                {"system": "connecting-party-manager/product_id", "value": "XXX-YYY"}
+                {"system": "connecting-party-manager/product_id", "value": "P.XXX-YYY"}
             ],
             "owner": {
                 "identifier": {
--- src/layers/domain/core/tests/test_product_id.py
+++ src/layers/domain/core/tests/test_product_id.py
@@ -1,10 +1,11 @@
 from domain.core.device_id import generate_device_key
+from domain.core.device_key import DeviceKeyType
 from domain.core.validation import PRODUCT_ID_REGEX
 
 
 def test__deterministic_generate():
-    a = generate_device_key("product")
-    b = generate_device_key("product")
+    a = generate_device_key(DeviceKeyType.PRODUCT_ID)
+    b = generate_device_key(DeviceKeyType.PRODUCT_ID)
 
     assert a != b
     assert PRODUCT_ID_REGEX.match(a) is not None
