233195d9fc172dbdac9f31c4847f75cb2632f3eb
megan-bower4 <megan.bower4@nhs.net>
2024-02-15 16:16:08 +0000

    Create spine_device questionnaire

---
+++ src/layers/domain/core/spine_device_questionnaire.py
@@ -0,0 +1,16 @@
+from .questionnaire import Questionnaire
+
+
+# You must bump the version if you edit this questionnaire
+def create_spine_device_questionnaire_v1():
+    spine_device_questionnaire = Questionnaire(name="spine_device", version=1)
+    spine_device_questionnaire.add_question(
+        name="ManufacturingOdsCode", answer_type=str
+    )
+    spine_device_questionnaire.add_question(
+        name="InteractionIds", answer_type=str, multiple=True
+    )
+    spine_device_questionnaire.add_question(name="Owner", answer_type=str)
+    spine_device_questionnaire.add_question(name="PartyKey", answer_type=str)
+
+    return spine_device_questionnaire
