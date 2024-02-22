6bf497535f5b081ff23cb35700dee2ef922c2785
megan-bower4 <megan.bower4@nhs.net>
2024-02-22 12:43:09 +0000

    sonarcloud suggestions

--- src/layers/domain/core/questionnaire_validation_custom_rules.py
+++ src/layers/domain/core/questionnaire_validation_custom_rules.py
@@ -9,10 +9,15 @@ def url(value):
 
 def empty_str(value):
 <<<<<<< HEAD
+<<<<<<< HEAD
     if isinstance(value, str) and len(value) != 0:
         raise ValueError("Expected empty string")
 =======
     if type(value) == str:
         if not len(value) == 0:
+=======
+    if isinstance(value, str):
+        if len(value) != 0:
+>>>>>>> d364e92 (sonarcloud suggestions)
             raise ValueError("Expected empty string")
 >>>>>>> dabc929 (Added multiple answer types functionality and empty string custom rule)
