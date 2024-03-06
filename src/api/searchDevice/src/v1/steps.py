304d68c043c93eeac1bd859144767bf0569beda0
jameslinnell <james.linnell2@nhs.net>
2024-03-06 16:29:57 +0000

    Fixed missing required query params

--- src/api/searchDevice/src/v1/steps.py
+++ src/api/searchDevice/src/v1/steps.py
@@ -1,16 +1,27 @@
 import os
 
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
+from event.json import json_loads
 from event.step_chain import StepChain
+
 
 def get_results(data, cache):
     event = APIGatewayProxyEvent(data[StepChain.INIT])
-    device_type = event.query_string_parameters["type"]
-    dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), os.path.join("tests", "test_data", "endpoints_response.json"))
+    device_type = event.query_string_parameters["device_type"]
+    dir_path = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))),
+        os.path.join("tests", "test_data", "endpoints_response.json"),
+    )
     if device_type.lower() == "product":
-        dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), os.path.join("tests", "test_data", "devices_response.json"))
-    with open(dir_path, 'r') as f:
-        return json.load(f)
+        dir_path = os.path.join(
+            os.path.dirname(
+                os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+            ),
+            os.path.join("tests", "test_data", "devices_response.json"),
+        )
+    with open(dir_path, "r") as f:
+        return json_loads(f)
+
 
 steps = [
     get_results,
