dd4d549a62a465aa9fe4910859819ebec093114c
jameslinnell <james.linnell2@nhs.net>
2024-03-07 17:27:54 +0000

    change returned to dict

--- src/api/searchDevice/src/v1/steps.py
+++ src/api/searchDevice/src/v1/steps.py
@@ -1,27 +1,17 @@
-import json
-import os
-
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
-from event.json import json_loads
 from event.step_chain import StepChain
+from searchDevice.src.data.response import devices, endpoints
 
 
 def get_results(data, cache):
     event = APIGatewayProxyEvent(data[StepChain.INIT])
     device_type = event.query_string_parameters["device_type"]
-    dir_path = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))),
-        os.path.join("src", "data", "endpoints_response.json"),
-    )
     if device_type.lower() == "product":
-        dir_path = os.path.join(
-            os.path.dirname(
-                os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-            ),
-            os.path.join("src", "data", "devices_response.json"),
-        )
-    with open(dir_path, "r") as f:
-        return json_loads(json.dumps(f))
+        return devices
+    elif device_type.lower() == "endpoint":
+        return endpoints
+
+    return {}
 
 
 steps = [
