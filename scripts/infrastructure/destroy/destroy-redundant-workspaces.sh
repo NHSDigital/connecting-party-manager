38af5abd02eb532ea7e6dc8fba09cf4749fe4903
jameslinnell <james.linnell2@nhs.net>
2024-03-15 16:13:10 +0000

    case insensitive destroy

--- scripts/infrastructure/destroy/destroy-redundant-workspaces.sh
+++ scripts/infrastructure/destroy/destroy-redundant-workspaces.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s nocasematch
 
 source ./scripts/infrastructure/terraform/terraform-utils.sh
 source ./scripts/infrastructure/terraform/terraform-constants.sh
@@ -82,7 +83,7 @@ function _destroy_redundant_workspaces() {
     # Print the matching object names
     for workspace in "${matching_objects[@]}"; do
         echo "Attempting to destroy workspace: $workspace"
-        bash ./scripts/infrastructure/terraform/terraform-commands.sh "destroy" "$ENVIRONMENT" "$workspace" "per_workspace" "-input=false -auto-approve -no-color"
+        #bash ./scripts/infrastructure/terraform/terraform-commands.sh "destroy" "$ENVIRONMENT" "$workspace" "per_workspace" "-input=false -auto-approve -no-color"
     done
 }
 
