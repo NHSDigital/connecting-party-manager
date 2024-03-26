e07d3d6de8b1c175827947f04d8eb3edca0bc264
megan-bower4 <megan.bower4@nhs.net>
2024-03-26 14:56:59 +0000

    feature/PI-213-connect_to_ldap update vpc endpoints to use resource

--- infrastructure/terraform/per_account/modules/vpc/lambda-connectivity-vpc.tf
+++ infrastructure/terraform/per_account/modules/vpc/lambda-connectivity-vpc.tf
@@ -33,3 +33,41 @@ resource "aws_subnet" "lambda-connectivity-private" {
     Tier        = "private"
   }
 }
+
+#------------------------------------------------------------------------------
+# Route Table
+#------------------------------------------------------------------------------
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.lambda-connectivity.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.subnet_count.private
+  subnet_id      = element(aws_subnet.lambda-connectivity-private, count.index).id
+  route_table_id = aws_route_table.private.id
+}
+
+#------------------------------------------------------------------------------
+# VPC endpoints
+#------------------------------------------------------------------------------
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.lambda-connectivity.id
+  service_name = "com.amazonaws.eu-west-2.s3"
+
+  tags = {
+    Name        = "s3-vpc-endpoint"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "hscn_endpoint" {
+  vpc_id       = aws_vpc.lambda-connectivity.id
+  service_name = data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string
+
+  tags = {
+    Name        = "ldap-vpc-endpoint"
+    Environment = var.environment
+  }
+}
