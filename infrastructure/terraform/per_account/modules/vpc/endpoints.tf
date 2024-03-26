b1f2c3ea1edd65fcb316b1aa28c753aeb94ce979
megan-bower4 <megan.bower4@nhs.net>
2024-03-26 14:23:02 +0000

    feature/PI-213-connect_to_ldap update vpc endpoints

--- infrastructure/terraform/per_account/modules/vpc/endpoints.tf
+++ infrastructure/terraform/per_account/modules/vpc/endpoints.tf
@@ -2,6 +2,9 @@ data "aws_secretsmanager_secret_version" "sds-ldap-endpoint" {
   secret_id = "${terraform.workspace}-sds-ldap-endpoint"
 }
 
+locals {
+  sds_ldap_endpoint_parse = split(".", data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string)
+}
 module "endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version = "5.5.3"
@@ -14,10 +17,11 @@ module "endpoints" {
       service = "s3"
       tags    = { Name = "s3-vpc-endpoint" }
     },
-    hscn_endpoint = {
-      service_name     = data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string
-      service_endpoint = data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string
-      tags             = { Name = "ldap-vpc-endpoint" }
+    # Retrieving service name from secretsmanager
+    local.sds_ldap_endpoint_parse[4] = {
+      service_name = data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string
+      # service_endpoint = data.aws_secretsmanager_secret_version.sds-ldap-endpoint.secret_string
+      tags = { Name = "ldap-vpc-endpoint" }
     }
   }
 
