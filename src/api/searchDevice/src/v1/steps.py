0b52f73a3dbdacc5d5e4961db4bef319ea7106fa
jameslinnell <james.linnell2@nhs.net>
2024-04-04 09:56:34 +0100

    Add steps to search

--- src/api/searchDevice/src/v1/steps.py
+++ src/api/searchDevice/src/v1/steps.py
@@ -1,7 +1,24 @@
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
+from domain.fhir_translation.device import create_fhir_model_from_device
+from domain.repository.device_repository import DeviceRepository
 from event.step_chain import StepChain
 
 from ..data.response import devices, endpoints
+
+
+def read_devices(data, cache):
+    event = APIGatewayProxyEvent(data[StepChain.INIT])
+    device_type = event.query_string_parameters["device_type"]
+    device_repo = DeviceRepository(
+        table_name=cache["DYNAMODB_TABLE"], dynamodb_client=cache["DYNAMODB_CLIENT"]
+    )
+    return device_repo.query_by_device_type(type=device_type)
+
+
+def device_to_fhir_org(data, cache) -> dict:
+    device = data[read_devices]
+    fhir_org = create_fhir_model_from_device(device=device)
+    return fhir_org.dict()
 
 
 def get_results(data, cache):
