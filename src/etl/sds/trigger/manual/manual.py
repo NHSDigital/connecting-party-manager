1cd8c0f764a683b398379c55dd5c7272244cf09a
jameslinnell <james.linnell2@nhs.net>
2024-07-02 14:20:27 +0100

    manually controlled

--- src/etl/sds/trigger/manual/manual.py
+++ src/etl/sds/trigger/manual/manual.py
@@ -1,5 +1,8 @@
+# import json
+
 import boto3
-from aws_lambda_powertools.utilities.data_classes import S3Event, event_source
+
+# f  # rom aws_lambda_powertools.utilities.data_classes import S3Event, event_source
 from etl_utils.trigger.logger import log_action
 from etl_utils.trigger.model import StateMachineInputType
 from etl_utils.trigger.notify import notify
@@ -11,7 +14,7 @@ from event.step_chain import StepChain
 from .steps import steps
 
 
-class BulkTriggerEnvironment(BaseEnvironment):
+class ManualTriggerEnvironment(BaseEnvironment):
     STATE_MACHINE_ARN: str
     NOTIFY_LAMBDA_ARN: str
     TABLE_NAME: str
@@ -21,7 +24,7 @@ S3_CLIENT = boto3.client("s3")
 DYNAMODB_CLIENT = dynamodb_client()
 STEP_FUNCTIONS_CLIENT = boto3.client("stepfunctions")
 LAMBDA_CLIENT = boto3.client("lambda")
-ENVIRONMENT = BulkTriggerEnvironment.build()
+ENVIRONMENT = ManualTriggerEnvironment.build()
 
 CACHE = {
     "s3_client": S3_CLIENT,
@@ -32,14 +35,27 @@ CACHE = {
 }
 
 
-@event_source(data_class=S3Event)
-def handler(event: S3Event, context):
+# @event_source(data_class=S3Event)
+def handler(event, context):
+    # print("Received event:", json.dumps(event))
     setup_logger(service_name=__file__)
     step_chain = StepChain(step_chain=steps, step_decorators=[log_action])
-    step_chain.run(init=(event.bucket_name, event.object_key), cache=CACHE)
+    step_chain.run(
+        init=(
+            event.get("bucket_name"),
+            event.get("object_key"),
+            event.get("manual_retry"),
+            event.get("etl_type"),
+        ),
+        cache=CACHE,
+    )
+    if event.get("etl_type").upper() == "BULK":
+        trigger_type = StateMachineInputType.BULK
+    elif event.get("etl_type").upper() == "UPDATE":
+        trigger_type = StateMachineInputType.UPDATE
     return notify(
         lambda_client=LAMBDA_CLIENT,
         function_name=ENVIRONMENT.NOTIFY_LAMBDA_ARN,
         result=step_chain.result,
-        trigger_type=StateMachineInputType.BULK,
+        trigger_type=trigger_type,
     )
--- src/etl/sds/trigger/manual/steps.py
+++ src/etl/sds/trigger/manual/steps.py
@@ -1,17 +1,21 @@
-# from pathlib import Path
-from typing import TYPE_CHECKING, TypedDict
+from pathlib import Path
 
 # from etl_utils.constants import WorkerKey
-# from etl_utils.trigger.model import StateMachineInput
+from etl_utils.trigger.model import StateMachineInput
+
 # from etl_utils.trigger.operations import start_execution, validate_state_keys_are_empty
-# from event.step_chain import StepChain
+from event.step_chain import StepChain
+
+# from typing import TYPE_CHECKING, TypedDict
+
 
 # from .operations import validate_database_is_empty, validate_no_changelog_number
 
-if TYPE_CHECKING:
-    from mypy_boto3_dynamodb import DynamoDBClient
-    from mypy_boto3_s3 import S3Client
-    from mypy_boto3_stepfunctions import SFNClient
+# if TYPE_CHECKING:
+#     from mypy_boto3_dynamodb import DynamoDBClient
+#     from mypy_boto3_s3 import S3Client
+#     from mypy_boto3_stepfunctions import SFNClient
+# from mypy_boto3_stepfunctions import SFNClient
 
 
 class Cache(TypedDict):
@@ -40,57 +44,73 @@ class Cache(TypedDict):
 #     )
 
 
-def _get_latest_state_machine_run(data, cache):
-    runs = SFNClient.list_map_runs()
-    # print(runs)
+# def _get_latest_state_machine_run(data, cache):
+#     statemachines = SFNClient.list_state_machines()
+#     print(statemachines)
+#     runs = SFNClient.list_executions(
+#         state_machine_arn="arn:aws:states:eu-west-2:660842439611:stateMachine:nhse-cpm--dev--sds",
+#         maxResults=2
+#     )
+#     print(runs)
+
+
+def _create_state_machine_input(data, cache):
+    _, source_key, manual_retry, etl_type = data[StepChain.INIT]
+    if etl_type.upper() != "BULK":
+        pass
+    return StateMachineInput.bulk(changelog_number=Path(source_key).stem)
 
 
 # def _create_state_machine_input(data, cache):
-#     _, source_key = data[StepChain.INIT]
-#     return StateMachineInput.bulk(changelog_number=Path(source_key).stem)
-
-
-# def _copy_to_state_machine(data, cache: Cache):
-#     source_bucket, source_key = data[StepChain.INIT]
-#     return cache["s3_client"].copy_object(
-#         Bucket=source_bucket,
-#         Key=WorkerKey.EXTRACT,
-#         CopySource=f"{source_bucket}/{source_key}",
+#     current_changelog_number = data[_get_current_changelog_number_from_s3]
+#     latest_changelog_number = data[_get_latest_changelog_number_from_ldap]
+#     return StateMachineInput.update(
+#         changelog_number_start=current_changelog_number,
+#         changelog_number_end=latest_changelog_number,
 #     )
 
 
-# def _copy_to_history(data, cache: Cache):
-#     source_bucket, source_key = data[StepChain.INIT]
-#     state_machine_input: StateMachineInput = data[_create_state_machine_input]
-#     return cache["s3_client"].copy_object(
-#         Bucket=source_bucket,
-#         Key=f"history/{state_machine_input.etl_type}/{source_key}",
-#         CopySource=f"{source_bucket}/{source_key}",
-#     )
+def _copy_to_state_machine(data, cache: Cache):
+    source_bucket, source_key = data[StepChain.INIT]
+    return cache["s3_client"].copy_object(
+        Bucket=source_bucket,
+        Key=WorkerKey.EXTRACT,
+        CopySource=f"{source_bucket}/{source_key}",
+    )
 
 
-# def _delete_object(data, cache: Cache):
-#     source_bucket, source_key = data[StepChain.INIT]
-#     return cache["s3_client"].delete_object(Bucket=source_bucket, Key=source_key)
+def _copy_to_history(data, cache: Cache):
+    source_bucket, source_key = data[StepChain.INIT]
+    state_machine_input: StateMachineInput = data[_create_state_machine_input]
+    return cache["s3_client"].copy_object(
+        Bucket=source_bucket,
+        Key=f"history/{state_machine_input.etl_type}/{source_key}",
+        CopySource=f"{source_bucket}/{source_key}",
+    )
 
 
-# def _start_execution(data, cache):
-#     state_machine_input: StateMachineInput = data[_create_state_machine_input]
-#     return start_execution(
-#         step_functions_client=cache["step_functions_client"],
-#         state_machine_arn=cache["state_machine_arn"],
-#         state_machine_input=state_machine_input,
-#     )
+def _delete_object(data, cache: Cache):
+    source_bucket, source_key = data[StepChain.INIT]
+    return cache["s3_client"].delete_object(Bucket=source_bucket, Key=source_key)
+
+
+def _start_execution(data, cache):
+    state_machine_input: StateMachineInput = data[_create_state_machine_input]
+    return start_execution(
+        step_functions_client=cache["step_functions_client"],
+        state_machine_arn=cache["state_machine_arn"],
+        state_machine_input=state_machine_input,
+    )
 
 
 steps = [
     # _validate_no_changelog_number,
     # _validate_state_keys_are_empty,
     # _validate_database_is_empty,
-    # _create_state_machine_input,
-    # _copy_to_state_machine,
-    # _copy_to_history,
-    # _delete_object,
-    # _start_execution,
-    _get_latest_state_machine_run
+    _create_state_machine_input,
+    _copy_to_state_machine,
+    _copy_to_history,
+    _delete_object,
+    _start_execution,
+    # _get_latest_state_machine_run
 ]
