0f020df2edf5dac3ccfd05dc4d92dce410f703d4
jameslinnell <james.linnell2@nhs.net>
2024-07-26 15:35:30 +0100

    return from Device Repository

--- src/layers/domain/repository/device_repository/v2.py
+++ src/layers/domain/repository/device_repository/v2.py
@@ -1,3 +1,6 @@
 class DeviceRepository(Repository[Device]):
+    def __init__(self):
+        super().__init__()
+
     def query_by_tag(self, **kwargs):
-        pass
+        return dict(nhs_as_svc_ia="Boo")
