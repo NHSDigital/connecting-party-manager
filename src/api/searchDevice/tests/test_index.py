b0989e9e172be901bac4d10b55607384edf3dd45
jameslinnell <james.linnell2@nhs.net>
2024-07-29 11:23:48 +0100

    Create 2 new search Endpoints

--- src/api/searchDevice/tests/test_index.py
+++ src/api/searchDevice/tests/test_index.py
@@ -14,7 +14,7 @@ from test_helpers.dynamodb import mock_table
 from test_helpers.response_assertions import _response_assertions
 from test_helpers.uuid import consistent_uuid
 
-from ..src.data.response_v2 import devices, endpoints
+from ..src.data.response import devices, endpoints
 
 TABLE_NAME = "hiya"
 
@@ -719,17 +719,7 @@ def test_index_multiple_active_devices(
     ],
 )
 def test_mock_is_returned(version, device):
-    with mock_table(TABLE_NAME) as client, mock.patch.dict(
-        os.environ,
-        {
-            "DYNAMODB_TABLE": TABLE_NAME,
-            "AWS_DEFAULT_REGION": "eu-west-2",
-        },
-        clear=True,
-    ):
-        from api.searchDevice.index import cache, handler
-
-        cache["DYNAMODB_CLIENT"] = client
+    from api.searchDevice.index import handler
 
     result = handler(
         event={
@@ -742,7 +732,7 @@ def test_mock_is_returned(version, device):
         }
     )
     result_body = json_loads(result["body"])
-    result_id = result_body[0].get("id")
+    result_id = result_body.get("id")
 
     expected_result = json.dumps(
         {"product": devices, "endpoint": endpoints}.get(
