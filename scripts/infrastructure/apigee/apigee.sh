cf4f43b1fad88fc05db4028cca1586c38ea75bb2
Rowan Gill <rowan.gill1@nhs.net>
2024-08-08 15:38:06 +0100

    feature/PI-305-add_product_to_apigee_app adjusted the script

--- scripts/infrastructure/apigee/apigee.sh
+++ scripts/infrastructure/apigee/apigee.sh
@@ -1,7 +1,11 @@
 set -e
 
+source ./scripts/infrastructure/terraform/terraform-utils.sh
+source ./scripts/infrastructure/terraform/terraform-constants.sh
+
 PATH_TO_HERE="scripts/infrastructure/apigee"
 APIGEE_DEPLOYMENT_ROLE="NHSDeploymentRole"
+
 
 if [[ -z ${WORKSPACE_OUTPUT_JSON} ]]; then
     echo "WORKSPACE_OUTPUT_JSON not set"
@@ -48,12 +52,16 @@ function get_apigee_environment(){
 }
 
 function get_access_token(){
+    echo "hje"
+    client_id=${1}
+    client_secret=${2}
     token_url='https://login.apigee.com/oauth/token'
 
     access_token=$(curl -s -X POST "$token_url" \
         -u "$client_id:$client_secret" \
         -d 'grant_type=client_credentials' | jq -r '.access_token')
 
+    echo "token $access_token"
     if [ "$access_token" == "null" ] || [ -z "$access_token" ]; then
       echo "Failed to obtain access token"
       exit 1
@@ -73,40 +81,62 @@ function attach_product(){
     _product_name="connecting-party-manager--$_apigee_environment--cpm-$_workspace_name--app-level0"
     _secret_name="$_aws_environment--apigee-app-client-info"
 
-        echo "
+    echo "
     Attaching product
     -------------------- ----------------------------------------
     workspace_name        ${_workspace_name}
     aws_environment       ${_aws_environment}
     apigee_environment    ${_apigee_environment}
-"
-    client_parameters=$(aws ssm get-parameter --name "apigee-app-client-info" --with-decryption --query "Parameter.Value" --output text)
-    client_id=$(echo "$client_parameters" | jq -r '.client_id')
-    client_secret=$(echo "$client_parameters" | jq -r '.client_secret')
-    client_key=$(echo "$client_parameters" | jq -r '.client_key')
-    email_that_owns_app="rowan.gill1@nhs.net"
-    app_name="CPM%20PTL" # Next time it might be worth to not have the space in there haha
+    product_name          ${_product_name}
+    secret_name           ${_secret_name}
+    "
 
-    _access_token=$(get_access_token ${client_id} ${client_secret})
+    terraform_role_name=NHSDeploymentRole
+    account_to_assume=$(_get_aws_account_id "$_aws_environment" "$PROFILE_PREFIX" "$VERSION")
+    role_arn="arn:aws:iam::${account_to_assume}:role/${terraform_role_name}"
+    session_name="attach-product-session"
+    duration_seconds=900
 
-    app_product=$(curl -X POST \
-        https://api.enterprise.apigee.com/v1/o/$_org_name/developers/$email_that_owns_app/apps/CPM%20PTL/keys/$client_key \
-        -H "Authorization: Bearer $_access_token" \
-        -H "Content-type:application/json" \
-        -d "{\"apiProducts\": [\"$_product_name\"]}")
+    assume_role_output=$(aws sts assume-role --role-arn "$role_arn" --role-session-name "$session_name" --duration-seconds "$duration_seconds")
+    # Check if the assume-role command was successful
+    if [ $? -eq 0 ]; then
 
-    # add_product_response=$(curl -s -X POST \
-    #   "https://api.enterprise.apigee.com/v1/organizations/$_org_name/apps/$_app_id" \
-    #   -H "Authorization: Bearer $_access_token" \
-    #   -H "Content-Type: application/json" \
-    #   -d "{\"apiProducts\": [\"$_product_name\"]}")
+        access_key=$(echo "$assume_role_output" | jq -r '.Credentials.AccessKeyId')
+        secret_key=$(echo "$assume_role_output" | jq -r '.Credentials.SecretAccessKey')
+        session_token=$(echo "$assume_role_output" | jq -r '.Credentials.SessionToken')
+        export AWS_ACCESS_KEY_ID="$access_key"
+        export AWS_SECRET_ACCESS_KEY="$secret_key"
+        export AWS_SESSION_TOKEN="$session_token"
 
-    if [[ "$add_product_response" == *"\"status\": \"error\""* ]]; then
-      echo "Failed to add product to the app"
-      echo "Response: $add_product_response"
-      exit 1
+        client_parameters=$(aws secretsmanager get-secret-value --secret-id $_secret_name --query SecretString --output text)
+        client_id=$(echo "$client_parameters" | jq -r '.client_id')
+        # client_key=$(echo "$client_parameters" | jq -r '.client_key')
+        email_that_owns_app="rowan.gill1@nhs.net"
+        app_name="CPM%20PTL" # Next time it might be worth to not have the space in there haha
+
+        token_response=$(proxygen pytest-nhsd-apim get-token)
+        token=$(echo "$token_response" | jq -r '.pytest_nhsd_apim_token')
+
+
+        app_product=$(curl -X POST \
+            https://api.enterprise.apigee.com/v1/organizations/$_org_name/developers/$email_that_owns_app/apps/$app_name/keys/$client_id \
+            -H "Authorization: Bearer $token" \
+            -H "Content-type:application/json" \
+            -d "{\"apiProducts\": [\"$_product_name\"]}")
+        echo "$app_product"
+        status=$(echo "$app_product" | jq -r '.status')
+        echo "$status"
+
+        if [ -z "$status" ] || [ "$status" != "approved" ]; then
+          echo "Failed to add product to the app"
+          echo "Response: $app_product"
+          exit 1
+        else
+          echo "Product added successfully!"
+        fi
     else
-      echo "Product added successfully!"
+        # Print an error message if assume-role command fails
+        echo "Error executing aws sts assume-role command"
     fi
 }
 
