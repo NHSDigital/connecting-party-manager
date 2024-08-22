e4cbcaea42c37468c064e0f5909337dbafe4c096
Rowan Gill <rowan.gill1@nhs.net>
2024-08-22 12:59:13 +0100

    release/2024-08-22 merge conflict fix

--- src/layers/domain/repository/device_repository/tests/v2/test_device_repository_v2.py
+++ src/layers/domain/repository/device_repository/tests/v2/test_device_repository_v2.py
@@ -80,11 +80,7 @@ def test__device_non_root_primary_keys():
         {
             "pk": {"S": "D#product_id#P.WWW-XXX"},
             "sk": {"S": "D#product_id#P.WWW-XXX"},
-        },
-        {
-            "pk": {"S": "DT#<<foo##bar>>"},
-            "sk": {"S": "D#123"},
-        },
+        }
     ]
 
 
