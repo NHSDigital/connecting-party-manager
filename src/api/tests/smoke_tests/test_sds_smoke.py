66ea085e65161734639bc97c53bf5171d2de3649
jameslinnell <james.linnell2@nhs.net>
2024-08-23 15:08:28 +0100

    SDS Smoke test

--- src/api/tests/smoke_tests/test_sds_smoke.py
+++ src/api/tests/smoke_tests/test_sds_smoke.py
@@ -11,11 +11,32 @@ from .utils import get_app_key, get_base_url
 def _request(request):
     url = f"{request['domain']}/spine-directory/FHIR/R4{request['path']}"
     res = requests.get(url, params=request["params"], headers=request["headers"])
-    return res.status_code
+    return res.status_code, res.json()
 
 
+@pytest.mark.parametrize(
+    "apirequest",
+    [
+        {
+            "path": "/Device",
+            "params": {
+                "identifier": "https://fhir.nhs.uk/Id/nhsServiceInteractionId|FOO",
+                "organization": "https://fhir.nhs.uk/Id/ods-organization-code|BAR",
+                "use_cpm": "iwanttogetdatafromcpm",
+            },
+        },
+        {
+            "path": "/Endpoint",
+            "params": {
+                "identifier": "https://fhir.nhs.uk/Id/nhsServiceInteractionId|FOO",
+                "organization": "https://fhir.nhs.uk/Id/ods-organization-code|BAR",
+                "use_cpm": "iwanttogetdatafromcpm",
+            },
+        },
+    ],
+)
 @pytest.mark.smoke
-def test_sds_smoke_tests():
+def test_sds_smoke_tests(apirequest):
     workspace = os.environ.get("WORKSPACE") or read_terraform_output("workspace.value")
     environment = os.environ.get("ACCOUNT") or read_terraform_output(
         "environment.value"
@@ -23,14 +44,9 @@ def test_sds_smoke_tests():
     domain_cpm = get_base_url(workspace=workspace, environment=environment)
     pos = domain_cpm.rfind("/")
     domain = domain_cpm[:pos]
-    request = {
-        "path": "/Device",
-        "params": {
-            "identifier": "https%3A%2F%2Ffhir.nhs.uk%2FId%2FnhsServiceInteractionId%7Curn%3Anhs%3Anames%3Aservices%3Apsisquery%3AFOO",
-            "organization": "https%3A%2F%2Ffhir.nhs.uk%2FId%2Fods-organization-code%7CBAR",
-        },
-        "domain": domain,
-        "headers": {"apikey": get_app_key(environment=environment, project="-sds")},
+    apirequest["domain"] = domain
+    apirequest["headers"] = {
+        "apikey": get_app_key(environment=environment, project="-sds")
     }
-    status_code = _request(request=request)
+    status_code, _ = _request(request=apirequest)
     assert status_code == 200
