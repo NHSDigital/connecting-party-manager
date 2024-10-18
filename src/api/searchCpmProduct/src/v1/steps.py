03f378114be2e0fac5ce3879db00f1926e2b4a5c
jameslinnell <james.linnell2@nhs.net>
2024-10-18 11:33:59 +0100

    Return tuple

--- src/api/searchCpmProduct/src/v1/steps.py
+++ src/api/searchCpmProduct/src/v1/steps.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 from typing import List
 
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
-from domain.core.cpm_product import CpmProduct
 from domain.repository.cpm_product_repository.v3 import CpmProductRepository
 from domain.repository.product_team_repository.v2 import ProductTeamRepository
 from event.step_chain import StepChain
@@ -21,7 +20,7 @@ def validate_product_team(data, cache) -> str:
     return product_team_repo.read(id=product_team_id)
 
 
-def query_products(data, cache) -> List[CpmProduct]:
+def query_products(data, cache) -> tuple[HTTPStatus, List[dict]]:
     product_team_id = data[parse_incoming_path_parameters]
     cpm_product_repo = CpmProductRepository(
         table_name=cache["DYNAMODB_TABLE"], dynamodb_client=cache["DYNAMODB_CLIENT"]
