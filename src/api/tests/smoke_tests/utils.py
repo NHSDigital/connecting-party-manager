65524dc35a88e7a49dda63f7e770a5b9571aba6f
jameslinnell <james.linnell2@nhs.net>
2024-10-25 19:17:23 +0100

    Better smoke tests

--- src/api/tests/smoke_tests/utils.py
+++ src/api/tests/smoke_tests/utils.py
@@ -48,8 +48,14 @@ def get_base_url(workspace: str, environment: str) -> str:
     return f"https://{base_url}/{api_name}"
 
 
-def is_2xx(status_code: int):
-    return 200 <= status_code < 300
+def is_2xx(status_code: int, expected_status_code: int):
+    return 200 <= status_code < 300 and status_code == expected_status_code
+
+
+def is_4xx(status_code: int, expected_status_code: int):
+    return (
+        400 <= status_code < 500 and status_code != 401
+    ) and status_code == expected_status_code
 
 
 def item_already_exists(response_json: dict) -> bool:
@@ -58,28 +64,64 @@ def item_already_exists(response_json: dict) -> bool:
     return _item_exists
 
 
-def execute_smoke_test(request_method: FunctionType, base_url: str, headers: str):
-    response: requests.Response = request_method(base_url=base_url, headers=headers)
+def raise_smoke_test_error(request_method, status_code, response_content):
+    raise SmokeTestError(
+        f"The smoke test for method '{request_method.__name__}' "
+        f"failed with status code {status_code} 😭😭\n"
+        f"{response_content}"
+    )
+
+
+def execute_smoke_test(
+    request_method: FunctionType, base_url: str, headers: str, request_details: list
+):
+    response: requests.Response = request_method(
+        base_url=base_url,
+        headers=headers,
+        path=request_details[0],
+        method=request_details[1],
+    )
 
     try:
         response_json: dict = response.json()
     except requests.JSONDecodeError:
         response_json = None
 
-    success = is_2xx(status_code=response.status_code) or (
-        response_json and item_already_exists(response_json=response_json)
+    success2xx = is_2xx(
+        status_code=response.status_code, expected_status_code=request_details[2]
     )
-    if success:
-        print(  # noqa: T201
-            f"🎉🎉 - 💨 Smoke 💨 test for method '{request_method.__name__}' has passed - 🎉🎉"
-        )
-    if not success:
-        if response.status_code == 401:
+    if success2xx:
+        if response_json == request_details[3]:
             print(  # noqa: T201
-                ">> Did you forget to manually register your app with the Apigee Product? <<"
+                f"🎉🎉 - 💨 Smoke 💨 test for method '{request_method.__name__}' has passed - 🎉🎉"
             )
-        raise SmokeTestError(
-            f"The smoke test for method '{request_method.__name__}' "
-            f"has failed with status code {response.status_code} 😭😭\n"
-            f"{response_json or response.text}"
-        )
+        else:
+            raise_smoke_test_error(
+                request_method, response.status_code, response_json or response.text
+            )
+
+    if not success2xx:
+        if is_4xx(
+            status_code=response.status_code, expected_status_code=request_details[2]
+        ):
+            error_assertion = all(
+                response_error["code"] in request_details[3]
+                and response_error["message"] in request_details[4]
+                for response_error in response_json.get("errors", [])
+            )
+            if error_assertion:
+                print(  # noqa: T201
+                    f"🎉🎉 - 💨 Smoke 💨 test for method '{request_method.__name__}' has passed - 🎉🎉"
+                )
+            else:
+                raise_smoke_test_error(
+                    request_method, response.status_code, response_json or response.text
+                )
+        else:
+            if response.status_code == 401:
+                print(  # noqa: T201
+                    ">> Did you forget to manually register your app with the Apigee Product? <<"
+                )
+            raise_smoke_test_error(
+                request_method, response.status_code, response_json or response.text
+            )
