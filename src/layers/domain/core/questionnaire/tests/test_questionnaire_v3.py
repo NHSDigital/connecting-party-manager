0b5b4257b665f3a8f14770fc24fe9f832bd6d45b
Joel Klinger <joel.klinger1@nhs.net>
2024-10-28 11:26:04 +0000

    [feature/PI-565-questionnaire_rethink] add more validation

--- src/layers/domain/core/questionnaire/tests/test_questionnaire_v3.py
+++ src/layers/domain/core/questionnaire/tests/test_questionnaire_v3.py
@@ -1,11 +1,13 @@
 import json
 
 import pytest
-from domain.core.enum import Status
-from domain.core.questionnaire.v3 import Questionnaire
+from domain.core.questionnaire.v3 import (
+    Questionnaire,
+    QuestionnaireResponseMissingValue,
+    QuestionnaireResponseValidationError,
+)
 from domain.core.timestamp import now
-from jsonschema import ValidationError as JsonSchemaValidationError
-from pydantic import ValidationError as PydanticValidationError
+from pydantic import ValidationError
 
 VALID_SCHEMA = {
     "$schema": "http://json-schema.org/draft-07/schema#",
@@ -51,13 +53,10 @@ def test_schema_validation_pass(data):
         name="foo", version="1", json_schema=json.dumps(VALID_SCHEMA)
     )
     response = questionnaire.validate(data=data)
-    assert response.name == "foo"
-    assert response.version == "1"
+    assert response.questionnaire_name == "foo"
+    assert response.questionnaire_version == "1"
     assert response.data == data
-    assert response.status is Status.ACTIVE
     assert response.created_on.date() == now().date()
-    assert response.updated_on is None
-    assert response.deleted_on is None
 
 
 @pytest.mark.parametrize(
@@ -77,17 +76,31 @@ def test_schema_validation_fail(data):
     questionnaire = Questionnaire(
         name="foo", version="1", json_schema=json.dumps(VALID_SCHEMA)
     )
-    with pytest.raises(JsonSchemaValidationError):
+    with pytest.raises(QuestionnaireResponseValidationError):
+        questionnaire.validate(data=data)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"size": 1},
+        {"colour": "white"},
+    ],
+)
+def test_schema_validation_missing_fail(data):
+    questionnaire = Questionnaire(
+        name="foo", version="1", json_schema=json.dumps(VALID_SCHEMA)
+    )
+    with pytest.raises(QuestionnaireResponseMissingValue):
         questionnaire.validate(data=data)
 
 
 @pytest.mark.parametrize(
     "schema",
     [
-        {},
         INVALID_SCHEMA,
     ],
 )
 def test_invalid_schema(schema):
-    with pytest.raises(PydanticValidationError):
+    with pytest.raises(ValidationError):
         Questionnaire(name="name", version="123", json_schema=json.dumps(schema))
--- src/layers/domain/core/questionnaire/v3.py
+++ src/layers/domain/core/questionnaire/v3.py
@@ -6,6 +6,14 @@ from domain.core.base import BaseModel
 from domain.core.timestamp import now
 from pydantic import Field, Json, validator
 
+REQUIRED = "required"
+
+
+class QuestionnaireResponseValidationError(Exception): ...
+
+
+class QuestionnaireResponseMissingValue(Exception): ...
+
 
 class Questionnaire(BaseModel):
     name: str
@@ -21,10 +29,26 @@ class Questionnaire(BaseModel):
         return json_schema
 
     def validate(self, data) -> "QuestionnaireResponse":
-        jsonschema.validate(instance=data, schema=self.json_schema)
+        try:
+            jsonschema.validate(instance=data, schema=self.json_schema)
+        except jsonschema.ValidationError as error:
+            *_, variable_name = error.schema_path
+            exception_type = (
+                QuestionnaireResponseMissingValue
+                if variable_name == REQUIRED
+                else QuestionnaireResponseValidationError
+            )
+            raise exception_type(
+                f"Failed to validate data against '{self.id}': {error.message}"
+            )
+
         return QuestionnaireResponse(
             questionnaire_name=self.name, questionnaire_version=self.version, data=data
         )
+
+    @property
+    def id(self) -> str:
+        return f"{self.name}/{self.version}"
 
 
 class QuestionnaireResponse(BaseModel):
@@ -33,3 +57,7 @@ class QuestionnaireResponse(BaseModel):
     questionnaire_version: str
     data: dict
     created_on: datetime = Field(default_factory=now)
+
+    @property
+    def questionnaire_id(self) -> str:
+        return f"{self.questionnaire_name}/{self.questionnaire_version}"
