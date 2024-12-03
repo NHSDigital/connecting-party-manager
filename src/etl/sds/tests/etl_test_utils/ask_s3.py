87e3e5465824282bc9ca1715ec59a396fdb0c2d3
Joel Klinger <joel.klinger1@nhs.net>
2024-12-03 15:54:22 +0000

    [feature/PI-618-bulk_etl_e2e] re-add triggers

--- src/etl/sds/tests/etl_test_utils/ask_s3.py
+++ src/etl/sds/tests/etl_test_utils/ask_s3.py
@@ -35,16 +35,12 @@ def ask_s3(
 def ask_s3_prefix(
     s3_client: S3Client, bucket: str, key_prefix: str, question: FunctionType = None
 ):
-    result = False
-
     response = s3_client.list_objects(Bucket=bucket, Prefix=key_prefix)
-
-    for item in response.get("Contents", []):
-        if item and question is not None:
-            result = ask_s3(
-                s3_client=s3_client, bucket=bucket, key=item["Key"], question=question
-            )
-    return result
+    keys = [item["Key"] for item in response.get("Contents", [])]
+    return any(
+        ask_s3(s3_client=s3_client, bucket=bucket, key=key, question=question)
+        for key in keys
+    )
 
 
 def was_trigger_key_deleted(s3_client, etl_config: EtlConfig):
@@ -60,7 +56,8 @@ def was_queue_history_created(
 ):
     return ask_s3_prefix(
         s3_client=s3_client,
-        key_prefix=f"{etl_config.state_machine_history_key_prefix}/{etl_config.etl_type}",
+        bucket=etl_config.bucket,
+        key_prefix=f"{etl_config.queue_history_key_prefix}{etl_config.etl_type}",
         question=lambda x: x == expected_content,
     )
 
@@ -70,7 +67,8 @@ def was_state_machine_history_created(
 ):
     return ask_s3_prefix(
         s3_client=s3_client,
-        key_prefix=f"{etl_config.queue_history_key_prefix}/{etl_config.etl_type}",
+        bucket=etl_config.bucket,
+        key_prefix=f"{etl_config.state_machine_history_key_prefix}{etl_config.etl_type}",
         question=lambda x: x == expected_content,
     )
 
@@ -120,4 +118,5 @@ def was_etl_state_lock_removed(s3_client, bucket) -> bool:
 
 
 def database_isnt_empty(db_client: "DynamoDBClient", table_name: str):
-    return db_client.scan(TableName=table_name, Limit=1, Select="COUNT")["Count"] == 1
+    count = db_client.scan(TableName=table_name, Limit=1, Select="COUNT")["Count"]
+    return count == 1
