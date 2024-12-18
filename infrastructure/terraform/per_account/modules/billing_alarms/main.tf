2b27c7b8ba57a39f0d3dd5e06a0d0ca173ad2bae
jameslinnell <jimlinnell@gmail.com>
2024-12-18 14:34:44 +0000

    Add slack sns sub

--- infrastructure/terraform/per_account/modules/billing_alarms/main.tf
+++ infrastructure/terraform/per_account/modules/billing_alarms/main.tf
@@ -1,13 +1,13 @@
-# module "notify_slack" {
-#   source  = "terraform-aws-modules/notify-slack/aws"
-#   version = "~> 5.0"
-#
-#   sns_topic_name = "billing-alert-slack-topic"
-#
-#   slack_webhook_url = "https://hooks.slack.com/services/AAA/BBB/CCC"
-#   slack_channel     = "aws-notification"
-#   slack_username    = "reporter"
-# }
+module "notify_slack" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 5.0"
+
+  sns_topic_name = "billing-alert-slack-topic"
+
+  slack_webhook_url = "https://hooks.slack.com/services/AAA/BBB/CCC"
+  slack_channel     = "aws-notification"
+  slack_username    = "reporter"
+}
 
 resource "aws_budgets_budget" "cpm_budget" {
   name         = "nhse-cpm-monthly-budget"
@@ -22,8 +22,8 @@ resource "aws_budgets_budget" "cpm_budget" {
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
-    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
+    # subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    subscriber_sns_topic_arns = [module.notify_slack.slack_topic_arn]
   }
 
   notification {
@@ -32,7 +32,8 @@ resource "aws_budgets_budget" "cpm_budget" {
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    subscriber_sns_topic_arns = [module.notify_slack.slack_topic_arn]
     # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
   }
 
@@ -42,7 +43,8 @@ resource "aws_budgets_budget" "cpm_budget" {
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    subscriber_sns_topic_arns = [module.notify_slack.slack_topic_arn]
     # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
   }
 
@@ -52,7 +54,8 @@ resource "aws_budgets_budget" "cpm_budget" {
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    subscriber_sns_topic_arns = [module.notify_slack.slack_topic_arn]
     # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
   }
 }
