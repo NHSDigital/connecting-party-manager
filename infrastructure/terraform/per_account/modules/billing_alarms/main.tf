9df3e65a5ef91cf58fcd31bc37a43109e0fcb195
jameslinnell <jimlinnell@gmail.com>
2024-12-18 13:57:44 +0000

    One budget to rule them all

--- infrastructure/terraform/per_account/modules/billing_alarms/main.tf
+++ infrastructure/terraform/per_account/modules/billing_alarms/main.tf
@@ -9,92 +9,104 @@
 #   slack_username    = "reporter"
 # }
 
-resource "aws_budgets_budget" "billing_alert_10" {
-  name         = "budget-10-monthly"
+resource "aws_budgets_budget" "cpm_budget" {
+  name         = "nhse-cpm-monthly-budget"
   budget_type  = "COST"
   limit_amount = var.limit
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
   notification {
-    comparison_operator = "GREATER_THAN"
-    threshold           = 10
-    threshold_type      = "PERCENTAGE"
-    notification_type   = "ACTUAL"
-    # subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.email_subscribers
+    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
+  }
+
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 75
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.email_subscribers
+    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
+  }
+
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 90
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.email_subscribers
+    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
+  }
+
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.email_subscribers
+    subscriber_sns_topic_arns  = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
     # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
   }
 }
-
-resource "aws_budgets_budget" "billing_alert_50" {
-  name         = "budget-50-monthly"
-  budget_type  = "COST"
-  limit_amount = var.limit
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator = "EQUAL_TO"
-    threshold           = 50
-    threshold_type      = "PERCENTAGE"
-    notification_type   = "ACTUAL"
-    # subscriber_email_addresses = var.email_subscribers
-    subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
-    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
-  }
-}
-
-resource "aws_budgets_budget" "billing_alert_75" {
-  name         = "budget-75-monthly"
-  budget_type  = "COST"
-  limit_amount = var.limit
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator       = "EQUAL_TO"
-    threshold                 = 75
-    threshold_type            = "PERCENTAGE"
-    notification_type         = "ACTUAL"
-    subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
-    # subscriber_email_addresses = var.email_subscribers
-    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
-  }
-}
-
-resource "aws_budgets_budget" "billing_alert_90" {
-  name         = "budget-590-monthly"
-  budget_type  = "COST"
-  limit_amount = var.limit
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator       = "EQUAL_TO"
-    threshold                 = 90
-    threshold_type            = "PERCENTAGE"
-    notification_type         = "ACTUAL"
-    subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
-    # subscriber_email_addresses = var.email_subscribers
-    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
-  }
-}
-
-resource "aws_budgets_budget" "billing_alert_100" {
-  name         = "budget-100-monthly"
-  budget_type  = "COST"
-  limit_amount = var.limit
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator       = "EQUAL_TO"
-    threshold                 = 100
-    threshold_type            = "PERCENTAGE"
-    notification_type         = "ACTUAL"
-    subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
-    # subscriber_email_addresses = var.email_subscribers
-    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
-  }
-}
+#
+# resource "aws_budgets_budget" "billing_alert_75" {
+#   name         = "budget-75-monthly"
+#   budget_type  = "COST"
+#   limit_amount = var.limit
+#   limit_unit   = "USD"
+#   time_unit    = "MONTHLY"
+#
+#   notification {
+#     comparison_operator       = "EQUAL_TO"
+#     threshold                 = 75
+#     threshold_type            = "PERCENTAGE"
+#     notification_type         = "ACTUAL"
+#     subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+#     subscriber_email_addresses = var.email_subscribers
+#     # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
+#   }
+# }
+#
+# resource "aws_budgets_budget" "billing_alert_90" {
+#   name         = "budget-590-monthly"
+#   budget_type  = "COST"
+#   limit_amount = var.limit
+#   limit_unit   = "USD"
+#   time_unit    = "MONTHLY"
+#
+#   notification {
+#     comparison_operator       = "EQUAL_TO"
+#     threshold                 = 90
+#     threshold_type            = "PERCENTAGE"
+#     notification_type         = "ACTUAL"
+#     subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+#     # subscriber_email_addresses = var.email_subscribers
+#     # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
+#   }
+# }
+#
+# resource "aws_budgets_budget" "billing_alert_100" {
+#   name         = "budget-100-monthly"
+#   budget_type  = "COST"
+#   limit_amount = var.limit
+#   limit_unit   = "USD"
+#   time_unit    = "MONTHLY"
+#
+#   notification {
+#     comparison_operator       = "EQUAL_TO"
+#     threshold                 = 100
+#     threshold_type            = "PERCENTAGE"
+#     notification_type         = "ACTUAL"
+#     subscriber_sns_topic_arns = ["arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"]
+#     # subscriber_email_addresses = var.email_subscribers
+#     # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
+#   }
+# }
