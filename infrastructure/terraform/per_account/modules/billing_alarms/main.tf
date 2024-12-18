6be891604f749766ec9c710844e40f9c4f298e34
jameslinnell <jimlinnell@gmail.com>
2024-12-18 11:48:13 +0000

    Add an SNS sub

--- infrastructure/terraform/per_account/modules/billing_alarms/main.tf
+++ infrastructure/terraform/per_account/modules/billing_alarms/main.tf
@@ -1,3 +1,14 @@
+# module "notify_slack" {
+#   source  = "terraform-aws-modules/notify-slack/aws"
+#   version = "~> 5.0"
+#
+#   sns_topic_name = "billing-alert-slack-topic"
+#
+#   slack_webhook_url = "https://hooks.slack.com/services/AAA/BBB/CCC"
+#   slack_channel     = "aws-notification"
+#   slack_username    = "reporter"
+# }
+
 resource "aws_budgets_budget" "billing_alert_50" {
   name         = "budget-50-monthly"
   budget_type  = "COST"
@@ -6,11 +17,13 @@ resource "aws_budgets_budget" "billing_alert_50" {
   time_unit    = "MONTHLY"
 
   notification {
-    comparison_operator        = "EQUAL_TO"
-    threshold                  = 50
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = var.email_subscribers
+    comparison_operator = "EQUAL_TO"
+    threshold           = 50
+    threshold_type      = "PERCENTAGE"
+    notification_type   = "ACTUAL"
+    # subscriber_email_addresses = var.email_subscribers
+    subscriber_sns_topic_arns = "arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"
+    # subscriber_sns_topic_arns = module.notify_slack.slack_topic_arn
   }
 
   tags = {
@@ -26,11 +39,13 @@ resource "aws_budgets_budget" "billing_alert_75" {
   time_unit    = "MONTHLY"
 
   notification {
-    comparison_operator        = "EQUAL_TO"
-    threshold                  = 75
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = var.email_subscribers
+    comparison_operator       = "EQUAL_TO"
+    threshold                 = 75
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = "arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"
+    # subscriber_email_addresses = var.email_subscribers
+    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
   }
 
   tags = {
@@ -46,11 +61,13 @@ resource "aws_budgets_budget" "billing_alert_90" {
   time_unit    = "MONTHLY"
 
   notification {
-    comparison_operator        = "EQUAL_TO"
-    threshold                  = 90
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = var.email_subscribers
+    comparison_operator       = "EQUAL_TO"
+    threshold                 = 90
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = "arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"
+    # subscriber_email_addresses = var.email_subscribers
+    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
   }
 
   tags = {
@@ -66,11 +83,13 @@ resource "aws_budgets_budget" "billing_alert_100" {
   time_unit    = "MONTHLY"
 
   notification {
-    comparison_operator        = "EQUAL_TO"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = var.email_subscribers
+    comparison_operator       = "EQUAL_TO"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = "arn:aws:sns:eu-west-2:660842439611:test-dev-billing-alarm"
+    # subscriber_email_addresses = var.email_subscribers
+    # subscriber_sns_topic_arns  = module.notify_slack.slack_topic_arn
   }
 
   tags = {
