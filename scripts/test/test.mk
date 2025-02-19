0ee3fd3ef55d05d9eba2a6c6a9cedea3a2718089
jameslinnell <jimlinnell@gmail.com>
2025-02-19 14:16:01 +0000

    Fix s3 integration test

--- scripts/test/test.mk
+++ scripts/test/test.mk
@@ -25,7 +25,7 @@ test--slow:  ## Run slow (pytest) tests
 	$(MAKE) _pytest _INTERNAL_FLAGS="-m 'slow' --ignore=archived_epr" _CACHE_CLEAR=$(_CACHE_CLEAR)
 
 test--s3: aws--login ## Run (pytest) tests that require s3 downloads
-	$(MAKE) _pytest _INTERNAL_FLAGS="-m 's3' $(_INTERNAL_FLAGS)" _CACHE_CLEAR=$(_CACHE_CLEAR) AWS_ACCESS_KEY_ID=$(AWS_ACCESS_KEY_ID) AWS_SECRET_ACCESS_KEY=$(AWS_SECRET_ACCESS_KEY) AWS_SESSION_TOKEN=$(AWS_SESSION_TOKEN)
+	$(MAKE) _pytest _INTERNAL_FLAGS="-m 's3' --ignore=archived_epr $(_INTERNAL_FLAGS)" _CACHE_CLEAR=$(_CACHE_CLEAR) AWS_ACCESS_KEY_ID=$(AWS_ACCESS_KEY_ID) AWS_SECRET_ACCESS_KEY=$(AWS_SECRET_ACCESS_KEY) AWS_SESSION_TOKEN=$(AWS_SESSION_TOKEN)
 
 test--smoke: aws--login ## Run end-to-end smoke tests (pytest)
 	AWS_DEFAULT_REGION=$(AWS_DEFAULT_REGION) AWS_ACCESS_KEY_ID=$(AWS_ACCESS_KEY_ID) AWS_SECRET_ACCESS_KEY=$(AWS_SECRET_ACCESS_KEY) AWS_SESSION_TOKEN=$(AWS_SESSION_TOKEN) WORKSPACE=$(WORKSPACE) ACCOUNT=$(ACCOUNT) poetry run python -m pytest $(PYTEST_FLAGS) -m 'smoke' --ignore=src/layers --ignore=src/etl --ignore=archived_epr/src $(_CACHE_CLEAR)
