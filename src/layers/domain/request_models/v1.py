12312c28d394e629b61931c7dc201c15d10ea5f6
jameslinnell <jimlinnell@gmail.com>
2025-02-24 11:33:47 +0000

    Fix request_models

--- src/layers/domain/request_models/v1.py
+++ src/layers/domain/request_models/v1.py
@@ -61,3 +61,25 @@ class CreateProductTeamIncomingParams(BaseModel, extra=Extra.forbid):
         self.keys = list(
             {frozenset(key.dict().items()): key for key in self.keys}.values()
         )
+
+
+class SearchProductQueryParams(BaseModel, extra=Extra.forbid):
+    product_team_id: str = None
+    organisation_code: str = None
+
+    @root_validator
+    def check_filters(cls, values: dict):
+        # Count the number of non-null parameters
+        non_empty_params = [
+            param for param in values.values() if param is not None and param != 0
+        ]
+
+        if len(non_empty_params) != 1:
+            raise ValueError(
+                f"Please provide exactly one valid query parameter: {ALLOWED_PRODUCT_SEARCH_PARAMS}."
+            )
+
+        return values
+
+    def get_non_null_params(self):
+        return self.dict(exclude_none=True)
