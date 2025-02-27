db73dbee97a9ca9511b01d345ff46ec118e5a8e6
jameslinnell <jimlinnell@gmail.com>
2025-02-27 11:17:45 +0000

    Remove test data related to EPR

---
+++ archived_epr/src_old/layers/domain/request_models/tests/test_parse_cpm_product_params.py
@@ -0,0 +1,29 @@
+import pytest
+from domain.request_models import CreateCpmProductIncomingParams
+from pydantic import ValidationError
+
+from test_helpers.sample_data import (
+    CPM_PRODUCT,
+    CPM_PRODUCT_EXTRA_PARAMS,
+    CPM_PRODUCT_NO_NAME,
+)
+
+
+def test_cpm_product():
+    product = CreateCpmProductIncomingParams(**CPM_PRODUCT)
+    assert isinstance(product, CreateCpmProductIncomingParams)
+    assert product.name == CPM_PRODUCT["name"]
+
+
+def test_validate_cpm_product_raises_no_extra_fields():
+    with pytest.raises(ValidationError) as exc:
+        CreateCpmProductIncomingParams(**CPM_PRODUCT_EXTRA_PARAMS)
+
+    assert exc.value.model is CreateCpmProductIncomingParams
+
+
+def test_validate_cpm_product_raises_no_name():
+    with pytest.raises(ValidationError) as exc:
+        CreateCpmProductIncomingParams(**CPM_PRODUCT_NO_NAME)
+
+    assert exc.value.model is CreateCpmProductIncomingParams
