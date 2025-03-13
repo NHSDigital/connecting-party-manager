48d45ecad74cf096069ddf88c0cfd7aa22fc0454
Megan <megan.bower4@nhs.net>
2025-03-13 15:37:34 +0000

    feature/PI-407-immutable_backups TEST - all backups permissions

--- infrastructure/terraform/modules/aws-backup-source/iam.tf
+++ infrastructure/terraform/modules/aws-backup-source/iam.tf
@@ -35,3 +35,26 @@ resource "aws_iam_role_policy_attachment" "s3_backup" {
   policy_arn = "arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Backup"
   role       = aws_iam_role.backup.name
 }
+
+
+resource "aws_iam_policy" "restore_testing_selection_permissions" {
+  name = "${local.resource_name_prefix}-source-account-backup-permissions"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "backup:*",
+          "cloudformation:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "source_account_backup_permissions" {
+  policy_arn = aws_iam_policy.restore_testing_selection_permissions.arn
+  role       = aws_iam_role.backup.name
+}
