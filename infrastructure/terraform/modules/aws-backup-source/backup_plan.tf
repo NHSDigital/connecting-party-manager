266c28f11640f5325bcb83b7a8c9a888f4834ee5
Megan <megan.bower4@nhs.net>
2025-03-14 13:50:34 +0000

    feature/PI-407-immutable_backups Enable source backup module without s3 or dynamo backups enabled

--- infrastructure/terraform/modules/aws-backup-source/backup_plan.tf
+++ infrastructure/terraform/modules/aws-backup-source/backup_plan.tf
@@ -1,33 +1,3 @@
-resource "aws_backup_plan" "default" {
-  name = "${local.resource_name_prefix}-plan"
-
-  dynamic "rule" {
-    for_each = var.backup_plan_config.rules
-    content {
-      recovery_point_tags = {
-        backup_rule_name = rule.value.name
-      }
-      rule_name                = rule.value.name
-      target_vault_name        = aws_backup_vault.main.name
-      schedule                 = rule.value.schedule
-      enable_continuous_backup = rule.value.enable_continuous_backup != null ? rule.value.enable_continuous_backup : null
-      lifecycle {
-        delete_after       = rule.value.lifecycle.delete_after != null ? rule.value.lifecycle.delete_after : null
-        cold_storage_after = rule.value.lifecycle.cold_storage_after != null ? rule.value.lifecycle.cold_storage_after : null
-      }
-      dynamic "copy_action" {
-        for_each = var.backup_copy_vault_arn != "" && var.backup_copy_vault_account_id != "" && rule.value.copy_action != null ? rule.value.copy_action : {}
-        content {
-          lifecycle {
-            delete_after = copy_action.value
-          }
-          destination_vault_arn = var.backup_copy_vault_arn
-        }
-      }
-    }
-  }
-}
-
 # this backup plan shouldn't include a continous backup rule as it isn't supported for DynamoDB
 resource "aws_backup_plan" "dynamodb" {
   count = var.backup_plan_config_dynamodb.enable ? 1 : 0
@@ -56,18 +26,6 @@ resource "aws_backup_plan" "dynamodb" {
         }
       }
     }
-  }
-}
-
-resource "aws_backup_selection" "default" {
-  iam_role_arn = aws_iam_role.backup.arn
-  name         = "${local.resource_name_prefix}-selection"
-  plan_id      = aws_backup_plan.default.id
-
-  selection_tag {
-    key   = var.backup_plan_config.selection_tag
-    type  = "STRINGEQUALS"
-    value = "True"
   }
 }
 
