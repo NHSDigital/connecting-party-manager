3f14b9fc062e8636a234e87ab3ec2d94a842c3c4
jameslinnell <jimlinnell@gmail.com>
2025-04-24 10:36:58 +0100

    Allow dependabot branch names

--- scripts/workflow/check-branch-name.sh
+++ scripts/workflow/check-branch-name.sh
@@ -6,14 +6,18 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 # Define a regular expression pattern to match the desired branch naming conventions
 feature_pattern="^feature/PI-[0-9]+[-][A-Za-z0-9\_]*$"
 release_pattern="^release/[0-9]{4}-[0-9]{2}-[0-9]{2}(-[a-z])?$"
+dependabot_pattern="^dependabot/.*"
 
 if [[ $current_branch =~ $feature_pattern ]]; then
-    echo "The branch '$current_branch' conforms to the feature branch pattern."
-    exit 0
+  echo "The branch '$current_branch' conforms to the feature branch pattern."
+  exit 0
 elif [[ $current_branch =~ $release_pattern ]]; then
-    echo "The branch '$current_branch' conforms to the release branch pattern."
-    exit 0
+  echo "The branch '$current_branch' conforms to the release branch pattern."
+  exit 0
+elif [[ $current_branch =~ $dependabot_pattern ]]; then
+  echo "The branch '$current_branch' conforms to the dependabot branch pattern."
+  exit 0
 else
-    echo "The branch '$current_branch' does not conform to any of the allowed patterns."
-    exit 1
+  echo "The branch '$current_branch' does not conform to any of the allowed patterns."
+  exit 1
 fi
